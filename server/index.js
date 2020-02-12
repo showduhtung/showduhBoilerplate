511e8b534e14e765865d51de76e1a47df3b08873
showduhtung <showduhtung@Shauns-MacBook-Pro.local>
2020-02-12 13:07:08 -0500

    macdev 2

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html');
 app.use(express.static(DIST_DIR));
 
 app.get('/', (req, res) => {
-    res.sendFile(HTML_FILE);
+  res.sendFile(HTML_FILE);
 });
 app.listen(port, function() {
-    console.log('Finance App listening on port: ' + port);
+  console.log('Finance App listening on port: ' + port);
 });
